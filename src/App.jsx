bda3a0bddf832a941717768157db034cc11e5939
pachizamora <maripaz.zamora@gmail.com>
2022-03-15 23:04:26 -0600

    Tenth Workshop React from Scratch

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+const App = function () {
+  return (
+    <div className="App">
+      <h1>Hola Mundo! ✌️</h1>
+    </div>
+  );
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+  entry: "./src/index.js",
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_module)/,
+        loader: "babel-loader",
+        options: {
+          presets: ["@babel/env"],
+        },
+      },
+      {
+        test: /\.css/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".wasm", ".ts", ".tsx", ".mjs", ".cjs", ".js", "jsx", ".json"],
+  },
+  output: {
+    path: path.join(__dirname, "dist/"),
+    publicPath: "/dist/",
+    filename: "bundle.js",
+  },
+  devServer: {
+    static: path.join(__dirname, "public/"),
+    port: 3000,
+    hot: true,
+  },
+  plugins: [new webpack.HotModuleReplacementPlugin()],
+};
